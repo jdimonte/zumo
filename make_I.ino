6c69027151a0d03e9dc9d2945780781181d5295c
jdimonte <67511953+jdimonte@users.noreply.github.com>
2021-04-15 01:37:51 -0400

    Added make_I and make_Z, updated forward_backwards
    Make_I is an implementation of the Zumo running in the shape of an I. Make_Z is an implementation of the Zumo running in the shape of a Z. Move_forward_move_backwards was updated to fix syntax errors.

---
+++ make_I.ino
@@ -0,0 +1,57 @@
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+
+const uint8_t motorSpeed = 400;
+
+void setup() {
+  lcd.clear();
+  lcd.gotoXY(0, 0);
+  lcd.print("Press A");
+}
+
+void loop() {
+  bool buttonPress = buttonA.getSingleDebouncedPress();
+
+  if (buttonPress) {
+    delay(1000);
+
+    //turn left
+    for (int speed = 0; speed <= 200; speed++)
+    {
+      motors.setRightSpeed(speed);
+      motors.setLeftSpeed(-speed);
+      delay(2);
+    }
+    //move forward
+    motors.setSpeeds(motorSpeed, motorSpeed);
+    delay(1000);
+    motors.setSpeeds(0, 0);
+    delay(500);
+    //turn right
+    for (int speed = 0; speed <= 400; speed++)
+    {
+      motors.setRightSpeed(-speed);
+      motors.setLeftSpeed(speed);
+      delay(2);
+    }
+    //move forward
+    motors.setSpeeds(motorSpeed, motorSpeed);
+    delay(1000);
+    motors.setSpeeds(0, 0);
+    delay(500);
+    //turn left
+    for (int speed = 0; speed <= 200; speed++)
+    {
+      motors.setRightSpeed(speed);
+      motors.setLeftSpeed(-speed);
+      delay(2);
+    }
+    //move forward;
+    motors.setSpeeds(motorSpeed, motorSpeed);
+    delay(1000);
+  }
+
+}
