f12d2a073561da706aee84b64856bc9e8298b70a
jdimonte <67511953+jdimonte@users.noreply.github.com>
2021-04-14 22:29:35 -0400

    Move Forward Move Backwards
    After you Press A, the Zumo moves forward and then moves backwards. Programmed with Arduino IDE.

---
+++ move_forward_move_backward.ino
@@ -0,0 +1,32 @@
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+
+const uint_t motorSpeed = 300;
+
+void setup() {
+  lcd.clear();
+  lcd.gotoXY(0, 0);
+  lcd.print("Press A");
+}
+
+void loop() {
+  bool buttonPress = buttonA.getSingleDeboucedPress();
+}
+
+if(buttonPress) {
+  delay(1000);
+
+  motors.setSpeeds(motorSpeed, motorSpeed);
+  delay(2000);
+
+  motors.setSpeeds(0, 0);
+  delay(500);
+
+  motors.setSpeeds(-motorSpeed, -motorSpeed);
+  delay(2000);
+
+  motors.setSpeeds(0, 0);
+}
