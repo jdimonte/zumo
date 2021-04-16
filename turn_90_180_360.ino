ed8ea801dd3fca736f604f5b8516b9e9772cfa5a
jdimonte <67511953+jdimonte@users.noreply.github.com>
2021-04-15 22:17:27 -0400

    Add files via upload

---
+++ turn_90_180_360.ino
@@ -0,0 +1,55 @@
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+
+const uint8_t motorSpeed = 400;
+
+void setup() {
+  lcd.clear();
+  lcd.gotoXY(0, 0);
+  lcd.print("Press A");
+}
+
+
+void loop() {
+  bool buttonPress = buttonA.getSingleDebouncedPress();
+
+  if (buttonPress) {
+    delay(1000);
+
+    turnLeft(255);
+
+    delay(50);
+    motors.setRightSpeed(0);
+    motors.setLeftSpeed(0);
+    delay(50);
+    
+    turnRight(265);
+
+    delay(50);
+    motors.setRightSpeed(0);
+    motors.setLeftSpeed(0);
+    delay(50);
+  }
+
+}
+
+void turnLeft(double n){
+  for (double speed = 0; speed <= (n/360)*510; speed++)
+    {
+      motors.setRightSpeed(speed);
+      motors.setLeftSpeed(-speed);
+      delay(2);
+    }
+}
+
+void turnRight(double n){
+  for (double speed = 0; speed <= (n/360)*510; speed++)
+    {
+      motors.setRightSpeed(-speed);
+      motors.setLeftSpeed(speed);
+      delay(2);
+    }
+}
